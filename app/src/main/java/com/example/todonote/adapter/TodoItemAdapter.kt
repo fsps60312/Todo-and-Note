d3e70c51e1e520f3853e14f935de5ccfb602bd5d
fsps60312 <fsps60312@yahoo.com.tw>
2022-04-26 22:27:13 +0800

    ConstraintLayout as a list item
    Signed-off-by: fsps60312 <fsps60312@yahoo.com.tw>

--- app/src/main/java/com/example/todonote/adapter/TodoItemAdapter.kt
+++ app/src/main/java/com/example/todonote/adapter/TodoItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todonote.R
@@ -12,18 +13,18 @@ import com.example.todonote.model.TodoItem
 class TodoItemAdapter(private val context: Context, private val dataset: List<TodoItem>) :
     RecyclerView.Adapter<TodoItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
+        val button: Button = view.findViewById(R.id.item_title)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text =
+        holder.button.text =
             context.getString(R.string.textview_format, item.datetime.toString(), item.title)
     }
 
